92320296bd7ace0ad2638a2adb2fb26e57049413
nklhrstv <me@nikolahristov.com>
2020-05-11 13:32:02 +0300

    basic link with cast context

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,21 @@ const React = require('react');
 const styles = require('./styles');
 
 const App = () => {
+    const context = React.useMemo(() => {
+        return cast.framework.CastReceiverContext.getInstance();
+    }, []);
+    React.useEffect(() => {
+        const onCustomMessage = (event) => {
+            context.sendCustomMessage('urn:x-cast:com.stremio', undefined, event);
+        };
+        context.addCustomMessageListener('urn:x-cast:com.stremio', onCustomMessage);
+        context.setLoggerLevel(process.env.DEBUG ? cast.framework.LoggerLevel.DEBUG : cast.framework.LoggerLevel.NONE);
+        context.start();
+        return () => {
+            context.removeCustomMessageListener('urn:x-cast:com.stremio', onCustomMessage);
+            context.stop();
+        };
+    }, []);
     return (
         <div className={styles['video-container']}>
             HI
