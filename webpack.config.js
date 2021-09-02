bbca9b1c9c93d72fdd644dd1c9b8b502e7612ce2
nklhrstv <me@nikolahristov.com>
2021-09-02 18:25:59 +0300

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,20 @@ const path = require('path');
 const { execSync } = require('child_process');
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const pachageJson = require('./package.json');
 
+const COMMIT_HASH = execSync('git rev-parse HEAD').toString().trim();
+
 module.exports = (env, argv) => ({
+    mode: argv.mode,
     entry: './src/index.js',
     output: {
-        path: path.join(__dirname, 'build')
+        path: path.join(__dirname, 'build'),
+        filename: `${COMMIT_HASH}/scripts/[name].js`
     },
     module: {
         rules: [
@@ -38,74 +42,111 @@ module.exports = (env, argv) => ({
                     {
                         loader: MiniCssExtractPlugin.loader,
                         options: {
-                            reloadAll: true
+                            esModule: false,
+                            modules: {
+                                namedExport: false
+                            }
                         }
                     },
                     {
                         loader: 'css-loader',
                         options: {
+                            esModule: false,
+                            importLoaders: 1,
                             modules: {
-                                localIdentName: '[local]'
+                                namedExport: false,
+                                localIdentName: '[local]-[hash:base64:5]'
                             }
                         }
                     },
                     {
                         loader: 'postcss-loader',
                         options: {
-                            ident: 'postcss-id',
-                            plugins: () => [
-                                require('cssnano')({
-                                    preset: [
-                                        'advanced',
-                                        {
-                                            autoprefixer: {
-                                                add: true,
-                                                remove: true,
-                                                flexbox: false,
-                                                grid: false
-                                            },
-                                            cssDeclarationSorter: true,
-                                            calc: false,
-                                            colormin: false,
-                                            convertValues: false,
-                                            discardComments: {
-                                                removeAll: true,
-                                            },
-                                            discardOverridden: false,
-                                            mergeIdents: false,
-                                            normalizeDisplayValues: false,
-                                            normalizePositions: false,
-                                            normalizeRepeatStyle: false,
-                                            normalizeUnicode: false,
-                                            normalizeUrl: false,
-                                            reduceIdents: false,
-                                            reduceInitial: false,
-                                            zindex: false
-                                        }
-                                    ]
-                                })
-                            ]
+                            postcssOptions: {
+                                plugins: [
+                                    require('cssnano')({
+                                        preset: [
+                                            'advanced',
+                                            {
+                                                autoprefixer: {
+                                                    add: true,
+                                                    remove: true,
+                                                    flexbox: false,
+                                                    grid: false
+                                                },
+                                                cssDeclarationSorter: true,
+                                                calc: false,
+                                                colormin: false,
+                                                convertValues: false,
+                                                discardComments: {
+                                                    removeAll: true,
+                                                },
+                                                discardOverridden: false,
+                                                discardUnused: false,
+                                                mergeIdents: false,
+                                                normalizeDisplayValues: false,
+                                                normalizePositions: false,
+                                                normalizeRepeatStyle: false,
+                                                normalizeUnicode: false,
+                                                normalizeUrl: false,
+                                                reduceIdents: false,
+                                                reduceInitial: false,
+                                                zindex: false
+                                            }
+                                        ]
+                                    })
+                                ]
+                            }
                         }
                     }
                 ]
-            }
+            },
+            {
+                test: /\.ttf$/,
+                exclude: /node_modules/,
+                loader: 'file-loader',
+                options: {
+                    esModule: false,
+                    name: '[name].[ext]',
+                    outputPath: `${COMMIT_HASH}/fonts`,
+                    publicPath: `/${COMMIT_HASH}/fonts`
+                }
+            },
+            {
+                test: /\.(png|jpe?g)$/,
+                exclude: /node_modules/,
+                loader: 'file-loader',
+                options: {
+                    esModule: false,
+                    name: '[name].[ext]',
+                    outputPath: `${COMMIT_HASH}/images`,
+                    publicPath: `/${COMMIT_HASH}/images`
+                }
+            },
         ]
     },
     resolve: {
-        extensions: ['.js', '.json', '.css']
+        extensions: ['.js', '.json', '.css'],
+        alias: {
+            'stremio-chromecast-receiver': path.join(__dirname, 'src')
+        }
     },
     devServer: {
         host: '0.0.0.0',
+        contentBase: false,
         hot: false,
-        inline: false
+        inline: false,
+        https: true,
+        liveReload: false
     },
     optimization: {
+        minimize: true,
         minimizer: [
             new TerserPlugin({
                 test: /\.js$/,
                 extractComments: false,
                 terserOptions: {
-                    ecma: 6,
+                    ecma: 5,
                     mangle: false,
                     warnings: false,
                     output: {
@@ -118,27 +159,29 @@ module.exports = (env, argv) => ({
         ]
     },
     plugins: [
+        new webpack.ProgressPlugin(),
         new webpack.EnvironmentPlugin({
+            ...env,
             DEBUG: argv.mode !== 'production',
             VERSION: pachageJson.version,
-            COMMIT_HASH: execSync('git rev-parse HEAD').toString(),
-            ...env
+            COMMIT_HASH
         }),
-        new webpack.ProgressPlugin(),
+        new webpack.ProvidePlugin({
+            Buffer: ['buffer', 'Buffer']
+        }),
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: ['*']
+        }),
         new CopyWebpackPlugin({
-            patterns: [
-                { from: 'images', to: 'images' }
-            ]
+            patterns: [{ from: 'favicons', to: `${COMMIT_HASH}/favicons` }]
+        }),
+        new MiniCssExtractPlugin({
+            filename: `${COMMIT_HASH}/styles/[name].css`
         }),
         new HtmlWebPackPlugin({
             template: './src/index.html',
-            inject: false
-        }),
-        new MiniCssExtractPlugin(),
-        new CleanWebpackPlugin({
-            verbose: true,
-            cleanOnceBeforeBuildPatterns: ['*'],
-            cleanAfterEveryBuildPatterns: ['./main.js', './main.css']
+            inject: false,
+            faviconsPath: `${COMMIT_HASH}/favicons`
         })
     ]
 });
